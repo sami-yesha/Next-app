519400f282d4c7d49b247ab25ba2abead87679de
sami <samuelyeshambel9@gmail.com>
2024-03-17 16:41:11 +0300

    .

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import Image from 'next/image'
-import CustomButton from './CustomButton'
-const Hero = () => {
-  return (
-    
-      <div className='hero'>
-        <div className='flex-1 pt-36 padding-x'>
-            <h1 className='hero__title'>
-                Find, book, or rent a car - quikly and easily!
-            </h1>
-            <p className='hero__sybtitile'>
-                Streamline your car rental experience with our effortless booking process.
-            </p>
-            <CustomButton/>
-        </div>
-      </div>
-    
-  )
-}
+"use client";
+import React from "react";
 
-export default Hero
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+const Hero = () => {
+  const handleScroll = () => {};
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, or rent a car - quikly and easily!
+        </h1>
+        <p className="hero__sybtitile">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
